4720afa078f1f2cd41794474a3cdd36a8bea9a22
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-22 17:25:44 +0530

    fallback error for meals and not found page added

---
+++ app/meals/error.js
@@ -0,0 +1,13 @@
+"use client";
+
+import "@/app/globals.css"
+
+const Error = () => {
+    return <main className="error">
+        <h1>An error occured!</h1>
+        <p>Failed to fetch meal data.Please try again later.</p>
+
+    </main>
+}
+
+export default Error;
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,20 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import MealsLoadingPage from "./loading-out";
 
-const MealsPage = async () => {
+
+const Meals = async () => {
     const meals = await getMeals();
+
+    return <MealsGrid meals={meals} />
+}
+
+
+const MealsPage = () => {
+
 
     return <>
         <header className={classes.header}>
@@ -21,7 +31,9 @@ const MealsPage = async () => {
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={meals} />
+            <Suspense fallback={<MealsLoadingPage />}>
+                <Meals />
+            </Suspense>
         </main>
     </>
 }
