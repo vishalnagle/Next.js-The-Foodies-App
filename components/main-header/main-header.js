02bc522981deecea70623b5b14d9ad149343dba7
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-22 14:01:13 +0530

    community page design added

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,32 +1,34 @@
+
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import Navlink from "./nav-link";
 
 
 const MainHeader = () => {
 
-    return<>
-    <MainHeaderBackground/>
-     <header className={classes.header}>
-        <Link className={classes.logo} href="/">
-            <Image src={logoImg} priority alt="A plate of food in it" />
-            NextLevel Food
-        </Link>
+    return <>
+        <MainHeaderBackground />
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <Image src={logoImg} priority alt="A plate of food in it" />
+                NextLevel Food
+            </Link>
 
-        <nav className={classes.nav}>
-            <ul>
-                <li>
-                    <Link href="/meals">Browse Meals</Link>
-                </li>
-                <li>
-                    <Link href="/community">Community</Link>
-                </li>
-            </ul>
-        </nav>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Navlink href="/meals" >Browse Meals</Navlink>
+                    </li>
+                    <li>
+                        <Navlink href="/community" >Foodies Community</Navlink>
+                    </li>
+                </ul>
+            </nav>
 
-    </header>
+        </header>
     </>
 }
 
