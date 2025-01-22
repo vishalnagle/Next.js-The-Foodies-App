a007b33ee24e0762e296f4fa9cdc08d96071eaa9
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-22 14:30:47 +0530

    MealsGrid and MealsItem components added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,26 @@
-const Meals = ()=>{
-    return <h1>Meals</h1>
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+const MealsPage = () => {
+    return <>
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created <span className={classes.highlight}>by you</span>
+            </h1>
+            <p>
+                Choose your favorite recipe and cook it yourself. It is easy and fun!
+            </p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">
+                    Share your favorite recipe
+                </Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={[]} />
+        </main>
+    </>
 }
 
-export default Meals;
+export default MealsPage;
