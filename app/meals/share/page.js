a6b93725d33f4314eadeddaeb0215c93e2837e5a
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-23 16:29:14 +0530

    Added form validations on share meal form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,15 @@
+"use client";
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
+import { useFormState } from 'react-dom';
+import { useActionState } from 'react';
 
 export default function ShareMealPage() {
+
+  const [state, formAction] = useActionState(shareMeal, { message: null })
 
   return (
     <>
@@ -13,7 +20,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,8 +49,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label={"Your image"} name={"image"} />
+          {state.message && <p style={{ color: "red" }}>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
